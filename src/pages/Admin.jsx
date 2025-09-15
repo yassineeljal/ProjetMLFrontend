d990f37f4697f500e59368828afd35ea600c79f0
Takfarinas <takfagaming@gmail.com>
2025-09-15 18:04:03 -0400

    travail sur la page admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -4,7 +4,8 @@ import {Link} from 'react-router-dom';
 import  {useState} from 'react'
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
-
+import { useEffect } from 'react';
+ 
 function Admin(props) {
     const[client, setClient] = useState({
         name:"",
@@ -12,54 +13,102 @@ function Admin(props) {
         gender:""
     });
 
+    const [deleteId, setDeleteId] = useState("");
+
+ 
     const setAttribut = (e) => {
         const value = e.target.value;
         setClient({...client, [e.target.name]: value})
-
+ 
     }
-
+ 
     const navigate = useNavigate();
-
+ 
     const submitClient = (e) =>{
         e.preventDefault();
-        axios.post("http://localhost:8888/addUser", client)
+        axios.post("http://localhost:8888/people/addUser", client)
             .then(() =>{
                 navigate("/")
             }).catch((error) =>{
             console.log(error);
         });
-
+ 
     }
+    
+ 
+    
+
+    //const supprimerClient = async (id) => {
+    //    try {
+    //        await axios.delete(`http://localhost:8888/people/user/${id}`);
+    //        await loadAllQuestions();
+    //    } catch (error) {
+    //        console.error("Erreur :", error);
+    //    }
+    //};
+
+    const supprimerClient = async (e) => {
+        e.preventDefault();        
+        if (!deleteId) return;       
+        try {
+            await axios.delete(`http://localhost:8888/people/user/${deleteId}`);
+            setDeleteId("");          
+        } catch (error) {
+            console.error("Erreur :", error);
+        }
+        };
+        
+ 
     return (
         <div className='tout'>
-
+ 
         <form className="form" onSubmit={(e) => submitClient(e)} method="post">
             <p className="title">Ajout</p>
-
+ 
             <label>
                 <input type="text"  className="input" name="name" id="name" placeholder="" required onChange={(e) => setAttribut(e)} value={client.name}/>
                 <span>Name</span>
             </label>
-
+ 
             <label>
                 <input type="text"  className="input" name="age" id="age" placeholder="" required onChange={(e) => setAttribut(e)} value={client.age}/>
                 <span>Age</span>
             </label>
-
+ 
             <label>
                 <input type="text"  className="input" name="gender" id="gender" placeholder="" required onChange={(e) => setAttribut(e)} value={client.gender}/>
                 <span>Gender</span>
             </label>
            
-           
-           
-                    
-            
             <button type="submit" className="submit">Submit</button>
         </form>
-        </div>
 
+         <form className="form" onSubmit={supprimerClient}>
+                <p className="title">Suppression</p>
+
+                <label>
+                    <input
+                        type="number"
+                        className="input"
+                        placeholder="ID du client"
+                        required
+                        onChange={(e) => setDeleteId(e.target.value)}
+                        value={deleteId}
+                    />
+                   
+                </label>
+
+                <button type="submit" className="submit">Supprimer</button>
+            </form>
+ 
+     
+       
+        </div>
+       
+ 
+       
+ 
     );
 }
-
+ 
 export default Admin;
